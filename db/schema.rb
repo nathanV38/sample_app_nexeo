2627484c737f8555a650c9e2e03ae3db3e4d618f
nathanV <nathan.vincent38@gmail.com>
2013-04-19 01:16:37 +0200

    ok done edit update index destroy

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130416113852) do
+ActiveRecord::Schema.define(:version => 20130418221015) do
 
   create_table "events", :force => true do |t|
     t.string   "name"
@@ -31,10 +31,11 @@ ActiveRecord::Schema.define(:version => 20130416113852) do
   create_table "users", :force => true do |t|
     t.string   "nom"
     t.string   "email"
-    t.datetime "created_at",         :null => false
-    t.datetime "updated_at",         :null => false
+    t.datetime "created_at",                            :null => false
+    t.datetime "updated_at",                            :null => false
     t.string   "encrypted_password"
     t.string   "salt"
+    t.boolean  "admin",              :default => false
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
